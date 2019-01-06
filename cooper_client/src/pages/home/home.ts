4580c8b91ecf7dc173342910524f8fa87a6869e8
Jonathan <jon1969edwards@gmail.com>
2019-01-06 19:34:21 +0100

    Modifies home and results pages, generates new 'providers' page

--- cooper_client/src/pages/home/home.ts
+++ cooper_client/src/pages/home/home.ts
@@ -1,16 +1,47 @@
+import { ResultsPage } from './../results/results';
+import { PerformanceDataProvider } from './../../providers/performance-data/performance-data';
+import { PersonProvider } from './../../providers/person/person';
 import { Component } from '@angular/core';
-import { NavController, ModalController } from 'ionic-angular';
+import { NavController } from 'ionic-angular';
+import { AlertController, ModalController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  user: any = {};
+  constructor(public navCtrl: NavController, public person: PersonProvider, 
+    private alertCtrl: AlertController, private performanceData: PerformanceDataProvider,
+    private modalCtrl:  ModalController) {
+    this.user = { distance: 1000, age: 20 };
+    }
 
-  constructor(public navCtrl: NavController) {
+      calculateOnly() {
+      if(this.user.gender) {
+        this.calculate()
+      } else {
+        let alert = this.alertCtrl.create({
+          title: 'Confirm gender',
+          message: 'Please confirm gender!',
+          buttons: ['Dismiss']
+        });
+        alert.present();
+      }
+    }
+    calculate() {
 
+      this.person.age = this.user.age;
+      this.person.gender = this.user.gender;
+      this.person.doAssessment(this.user.distance);
   }
-showResults() {
+  showResults() {
     this.modalCtrl.create(ResultsPage).present();
+  }
+
+  saveResults() {
+    this.performanceData
+        .saveData({ performance_data: { data: { message: this.person.assessmentMessage } } })
+        .subscribe(data => console.log(data))
   }
 }
--- src/pages/results/results.ts
+++ src/pages/results/results.ts
@@ -1,24 +1,66 @@
+import { PerformanceDataProvider } from './../../providers/performance-data/performance-data';
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, ViewController } from 'ionic-angular';
 
-/**
- * Generated class for the ResultsPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @Component({
   selector: 'page-results',
   templateUrl: 'results.html',
 })
 export class ResultsPage {
+  results = [];
+  labels = [];
+  data = [];
+  doughnutChartType: string = 'doughnut';
+  radarChartType: string = 'radar';
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  view: string = 'data';
+
+  constructor(private perfomanceData: PerformanceDataProvider,
+    public navCtrl: NavController, public navParams: NavParams, public viewCtrl: ViewController) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad ResultsPage');
+    this.perfomanceData
+      .getResults()
+        .subscribe(data => {
+          this.results = data.entries;
+          this.labels = this.getLabels(this.results);
+          this.labels.forEach(label => {
+            this.data.push(this.getCount(this.results, label))
+          })
+        });
   }
 
+  getLabels(collection: any) {
+    let uniqueLabels = [];
+
+    collection.forEach(entry => {
+      if(entry.data.message && uniqueLabels.indexOf(entry.data.message) === -1) {
+        uniqueLabels.push(entry.data.message);
+      }
+    })
+    return uniqueLabels;
+  }
+
+  getCount(collection: any, value: any) {
+    let count = 0;
+
+    collection.forEach(entry => {
+      count += entry.data.message == value ? 1: 0;
+    })
+    return count;
+  }
+
+  chartClicked(event: any): void {
+    console.log(event);
+}
+
+  chartHovered(event: any): void {
+    console.log(event);
+  }
+
+  closeResults() {
+    this.viewCtrl.dismiss();
+  }
 }
